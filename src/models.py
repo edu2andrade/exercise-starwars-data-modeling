510eb56a29c9a62dcf7be8830eec8e86729d2de8
Eduardo Andrade <edu2andrade@gmail.com>
2023-03-25 11:39:15 +0000

    feat: ids corrected

--- src/models.py
+++ src/models.py
@@ -10,8 +10,8 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    user_id = Column(Integer, primary_key=True)
-    user_name = Column(String(120), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(120), nullable=False)
     email = Column(String(120), nullable=False)
     password = Column(String(250), nullable=False)
 
